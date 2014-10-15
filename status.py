9316f7f3095722402dbf6d9d357a8cef0181fb4a
Nicholas Mykulowycz <nanosecrepair@gmail.com>
2014-10-15 14:06:26 -0400

    Create status.py

---
+++ status.py
@@ -0,0 +1,24 @@
+#!/usr/bin/env python
+
+#Written by Nicholas Mykulowycz
+#Created on Oct 15, 2014
+#script for checking status of switch.py script
+
+import os
+import sys
+import time
+from time import sleep
+
+#pin definitions
+ON_LED = 27
+DIM_LED = 22
+COLOR_LED = 17
+
+#set all first LED to on - indicates script ran
+GPIO.output(ON_LED, True)
+
+#set second LED to on - indicates bridge connection was set up
+GPIO.output(COLOR_LED, True)
+
+#the third LED is on - indicates the main loop was reached
+GPIO.output(DIM_LED, True)
