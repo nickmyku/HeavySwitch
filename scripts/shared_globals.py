38cd991fb3f978f2370c01f3ee35eaa9da4ead65
Nicholas Mykulowycz <nanosecrepair@gmail.com>
2014-10-22 14:24:22 -0400

    Update shared_globals.py

--- scripts/shared_globals.py
+++ scripts/shared_globals.py
@@ -1,6 +1,10 @@
 # set path for state file
 STATE_PATH = '/home/pi/HeavySwitch/log/state'
 
+
+# email credientials
+
+
 # input pin definitions
 ON_PIN = 24
 COLOR_PIN = 23
@@ -11,4 +15,22 @@ DIM_LED = 22
 COLOR_LED = 17
 
 
-# email credientials
+# on light parameters
+on_Hue = 35000		#hue value, from 0 to 65280
+on_Sat = 255		#saturation value, from 0 to 255, higher the more colorful
+on_Bri = 255		#brightness value, from 0 to 255, higher is brighter
+# color light parameters
+color_Hue_1 = 58000	#hue value, from 0 to 65280
+color_Sat_1 = 255	#saturation value, from 0 to 255, higher the more colorful
+color_Bri_1 = 127		#brightness value, from 0 to 255, higher is brighter
+color_Hue_2 = 47000	#hue value, from 0 to 65280
+color_Sat_2 = 255	#saturation value, from 0 to 255, higher the more colorful
+color_Bri_2 = 200	#brightness value, from 0 to 255, higher is brighter
+color_Hue_3 = 58000	#hue value, from 0 to 65280
+color_Sat_3 = 255	#saturation value, from 0 to 255, higher the more colorful
+color_Bri_3 = 127		#brightness value, from 0 to 255, higher is brighter
+# dim light parameters
+dim_Hue = 10000		#hue value, from 0 to 65280
+dim_Sat = 100		#saturation value, from 0 to 255, higher the more colorful
+dim_Bri = 1		#brightness value, from 0 to 255, higher is brighter
+
