ad8026326ace61c9c320baa36abec9ead3a5de2f
Nicholas Mykulowycz <nanosecrepair@gmail.com>
2014-10-22 14:23:08 -0400

    Create shared_globals.py

---
+++ scripts/shared_globals.py
@@ -0,0 +1,14 @@
+# set path for state file
+STATE_PATH = '/home/pi/HeavySwitch/log/state'
+
+# input pin definitions
+ON_PIN = 24
+COLOR_PIN = 23
+DIM_PIN = 25
+# led pin definitions
+ON_LED = 27
+DIM_LED = 22
+COLOR_LED = 17
+
+
+# email credientials
