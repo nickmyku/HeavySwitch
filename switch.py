66c608d5437fa4296a1862f152393593695cda3e
nickmyku <nanosecrepair@gmail.com>
2014-10-22 03:04:59 +0000

    fixed a ton of syntax errors, removed pygame it was causing too many problems

--- switch.py
+++ switch.py
@@ -6,10 +6,10 @@
 
 import os
 import sys
-import time
 import pygame
 from pygame.locals import *
 from time import sleep
+from time import time
 from phue import Bridge
 import RPi.GPIO as GPIO
 from subprocess import call
@@ -29,9 +29,9 @@ state_data = ['0','script_terminated','bridge_disconnected','loop_terminated']
 	#return string
 	
 #function for writing to state file
-def writeStateFile(array)
+def writeStateFile(array):
 	fileObj = open('log/state', 'wb')
-	pickle.dump(array)	
+	pickle.dump(array,fileObj)	
 	#then imediately close the file
 	fileObj.close()
 
@@ -95,10 +95,10 @@ state_data[2] = 'bridge_connected'
 writeStateFile(state_data)
 
 #intialize pygame
-pygame.init()
-screen = pygame.display.set_mode((640,480))	#need to set display to catch key presses
-pygame.display.set_caption('HeavySwitch')
-pygame.mouse.set_visible(0)
+#pygame.init()
+#screen = pygame.display.set_mode((640,480))	#need to set display to catch key presses
+#pygame.display.set_caption('HeavySwitch')
+#pygame.mouse.set_visible(0)
 
 def setLight(light_num, on_val, hue_val, sat_val, bright_val):
 	lights[light_num].on = on_val	#light must be on to change paramters
@@ -134,8 +134,8 @@ def setAll(on_val, hue_val, sat_val, bright_val):
     return;
 
 def buttonHeld(pin):
-    start_time = time.time()
-    curr_time = time.time()
+    start_time = time()
+    curr_time = time()
     
     #set the pin numbers of the other buttons based on the pin that was fed into the function
     if(pin == ON_PIN):
@@ -152,7 +152,7 @@ def buttonHeld(pin):
     #and the button is still being held AND no other buttons are pressed
     while((curr_time-start_time < hold_time) and GPIO.input(pin) == False and GPIO.input(alt_pin_1) == True and GPIO.input(alt_pin_2) == True):
         sleep(0.05);
-        curr_time = time.time()
+        curr_time = time()
 
     #if pin is still held after loop exits button hold detected AND no other buttons are pressed
     #turn off all lights and return true
@@ -164,8 +164,8 @@ def buttonHeld(pin):
     else:
         return False;
 
-
-print("\npress Ctrl+C or press all buttons to terminate script")
+print "\nswitch.py running..."
+print "Press Ctrl+C or all physical buttons to terminate script\n"
 
 def setLightsON():
 	global on_state
@@ -208,11 +208,11 @@ def setLightsDIM():
 	return;
 
 def setStateLabel(text_str):
-	screen.fill(pygame.Color("black"))	#erase screen
-	font = pygame.font.Font(None, 25)	#set font parameters
-	label_text = font.render(str(text_str), True, (255,255,255))	#assign text, make visible, make color white
-	screen.blit(label_text, (0,10))		#set label position
-	pygame.display.update()			#update the screen
+	#screen.fill(pygame.Color("black"))	#erase screen
+	#font = pygame.font.Font(None, 25)	#set font parameters
+	#label_text = font.render(str(text_str), True, (255,255,255))	#assign text, make visible, make color white
+	#screen.blit(label_text, (0,10))		#set label position
+	#pygame.display.update()			#update the screen
 	print text_str
 	return;
 
@@ -247,28 +247,28 @@ while True:
 	#write to file
 	writeStateFile(state_data)
 	#key press handler using pygame events
-	for event in pygame.event.get():
-		if event.type == pygame.KEYDOWN:
-			if event.key == pygame.K_SPACE:
-				allOff()
+	#for event in pygame.event.get():
+		#if event.type == pygame.KEYDOWN:
+			#if event.key == pygame.K_SPACE:
+				#allOff()
 			#q key turns on full brightness
-			if event.key == pygame.K_q:
-				if(on_state == False):
-					setLightsON()
-				else:
-					allOff()
+			#if event.key == pygame.K_q:
+				#if(on_state == False):
+					#setLightsON()
+				#else:
+					#allOff()
 			#a key turns lights on color mode
-			if event.key == pygame.K_a:
-				if(color_state == False):
-					setLightsCOLOR()
-				else:
-					allOff()
+			#if event.key == pygame.K_a:
+				#if(color_state == False):
+					#setLightsCOLOR()
+				#else:
+					#allOff()
 			#z key turn lights on in dim mode
-			if event.key == pygame.K_z:
-				if(dim_state == False):
-					setLightsDIM()
-				else:
-					allOff()
+			#if event.key == pygame.K_z:
+				#if(dim_state == False):
+					#setLightsDIM()
+				#else:
+					#allOff()
         #if "ON" button pressed but not held and no other buttons were pressed - then set lights to "ON" profile
 	if(GPIO.input(ON_PIN) == False and buttonHeld(ON_PIN) == False and GPIO.input(COLOR_PIN) == True and GPIO.input(DIM_PIN) == True):
            #if this light combo is not already on then turn it on
@@ -320,10 +320,11 @@ while True:
 
 #initialize state data array
 state_data[0] = str(time())
-state_date[1] = 'script_terminated'
+state_data[1] = 'script_terminated'
 state_data[2] = 'bridge_disconnected'
 state_data[3] = 'loop_terminated'
 #write to file
 writeStateFile(state_data)
 
+print "switch.py terminated..."
 
